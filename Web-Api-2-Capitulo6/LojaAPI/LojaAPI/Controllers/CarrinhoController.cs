fb043775cd58239c527f244897d3c016849d6b0c
caiocmsouza <caiocesar.msouza@gmail.com>
2017-02-21 15:56:39 -0300

    aula6

---
+++ Web-Api-2-Capitulo6/LojaAPI/LojaAPI/Controllers/CarrinhoController.cs
@@ -0,0 +1,42 @@
+﻿using Loja.DAO;
+using Loja.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LojaAPI.Controllers
+{
+    public class CarrinhoController : ApiController
+    {
+        public HttpResponseMessage Get(int id)
+        {
+            try
+            {
+                var dao = new CarrinhoDAO();
+                var carrinho = dao.Busca(id);
+                return Request.CreateResponse(HttpStatusCode.OK, carrinho);
+            }
+            catch (KeyNotFoundException)
+            {
+                var mensagem = string.Format("O carrinho {0} não foi encontrado", id);
+                var error = new HttpError(mensagem);
+                return Request.CreateResponse(HttpStatusCode.NotFound, error);
+            }
+        }
+
+        public HttpResponseMessage Post([FromBody] Carrinho carrinho)
+        {
+            CarrinhoDAO dao = new CarrinhoDAO();
+            dao.Adiciona(carrinho);
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created);
+            string location = Url.Link("DefaultApi", new { controller = "carrinho", id = carrinho.Id });
+            response.Headers.Location = new Uri(location);
+
+            return response;
+        }
+    }
+}
